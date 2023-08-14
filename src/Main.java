f14f12402e53959eda8239faa2b36b72eb5e2263
WhiteMeaning <riddick928@aol.com>
2023-08-14 13:54:07 +0300

    update scanner function

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner s = new Scanner(System.in);
-        String a = s.next();
+        String a = s.nextLine();
         System.out.println(a);
 
 
