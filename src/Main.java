43f901ecaff2dd11e709a894ca543c721eafe113
WhiteMeaning <riddick928@aol.com>
2023-08-14 13:57:03 +0300

    hide scanner function

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner s = new Scanner(System.in);
-        String a = s.nextLine();
-        System.out.println(a);
+//        Scanner s = new Scanner(System.in);
+//        String a = s.nextLine();
+//        System.out.println(a);
+//
 
 
         }
