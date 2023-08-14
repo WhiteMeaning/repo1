3f1f549a635094bf8bfc8ec1a4cce6681b6b273b
WhiteMeaning <riddick928@aol.com>
2023-08-14 13:51:06 +0300

    add scanner function

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
+
+        Scanner s = new Scanner(System.in);
+        String a = s.next();
+        System.out.println(a);
+
 
         }
     }
