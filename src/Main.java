7463e72a1e28102615f74e4d6269cfb8fd38f03b
WhiteMeaning <riddick928@aol.com>
2023-08-14 14:05:34 +0300

    add boolean function and hide

--- src/Main.java
+++ src/Main.java
@@ -13,5 +13,17 @@ public class Main {
 //        String d = " Diablo is bad";
 //        System.out.println(d.length());
 
+
+//        String a = " Diablo is bad";
+//        boolean per = false;
+//        for(int i = 0;i<a.length();i++){
+//            if(a.charAt(i) == 'e'){
+//                per=true;
+//                break;
+//            }
+//        }
+//        System.out.println(per);
+
+
         }
     }
