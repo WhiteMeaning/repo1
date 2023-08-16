184974751711a2c7141c292e54c1b20490c0c99a
WhiteMeaning <riddick928@aol.com>
2023-08-16 14:56:06 +0300

    practice 2 started "while"

--- src/while1.java
+++ src/while1.java
@@ -1,13 +1,38 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class while1 {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> arrayList = new ArrayList<>(5);
+
+
+        while (scanner.hasNext()){
+            arrayList.add(scanner.nextInt());
+            System.out.println(arrayList);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //        int i = 0;
 //        while (i<=5){
-//            String ss = "Something";
-//            System.out.println(ss);
+//            String ss = "Something ";
+//            System.out.println(ss + i);
 //            i++;
 //
 //        }
 
-    }
-}
+    }}
+
