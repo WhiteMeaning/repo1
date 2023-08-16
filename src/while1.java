18d6673b0130047168f769c8d3ce8093550dd352
WhiteMeaning <riddick928@aol.com>
2023-08-16 13:26:00 +0300

    practice 1 completed "while"

--- src/while1.java
+++ src/while1.java
@@ -1,13 +1,13 @@
 public class while1 {
     public static void main(String[] args) {
 
-        int i = 0;
-        while (i<=5){
-            String ss = "Something";
-            System.out.println(ss);
-            i++;
-
-        }
+//        int i = 0;
+//        while (i<=5){
+//            String ss = "Something";
+//            System.out.println(ss);
+//            i++;
+//
+//        }
 
     }
 }
