b529410a93e92cdcbd9a12af0ee4860f9a8296fc
WhiteMeaning <riddick928@aol.com>
2023-08-17 11:50:18 +0300

    practice 2 completed "while"

--- src/while1.java
+++ src/while1.java
@@ -4,25 +4,27 @@ import java.util.Scanner;
 public class while1 {
     public static void main(String[] args) {
 
+        int i = 0;
+
         Scanner scanner = new Scanner(System.in);
         ArrayList<Integer> arrayList = new ArrayList<>(5);
 
-
-        while (scanner.hasNext()){
+        while(i <=4){
             arrayList.add(scanner.nextInt());
-            System.out.println(arrayList);
+            i++;
         }
+        System.out.println(arrayList);
 
 
 
-
-
-
-
-
-
-
-
+//
+//
+//
+//            while (scanner.hasNext()){
+//            arrayList.add(scanner.nextInt());
+//            i++;
+//
+//        }
 
 
 
