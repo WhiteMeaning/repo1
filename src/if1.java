73190fabaaa364a5c0e5674eb4ce8f3a01f4da81
WhiteMeaning <riddick928@aol.com>
2023-09-05 15:41:51 +0300

    fist practice with if ends

--- src/if1.java
+++ src/if1.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class if1 {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+
+        if(num1<num2){
+            System.out.println("This is minimum value: "+num1);
+            }if (num1>num2){
+            System.out.println("This is maximum value: "+num1);
+
+        }
+
 
     }
 }
